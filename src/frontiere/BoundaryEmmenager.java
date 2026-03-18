20650c780ac702c91d339e8992cba280ceec22d7
annad31 <anna.denis@utoulouse.fr>
2026-03-18 11:09:29 +0100

    TP2-2 fini

--- src/frontiere/BoundaryEmmenager.java
+++ src/frontiere/BoundaryEmmenager.java
@@ -27,12 +27,14 @@ public class BoundaryEmmenager {
 					break;
 
 				case 2:
-					//TODO a completer
+					System.out.println("Bienvenue villageois " + nomVisiteur + ".");
+					int force=-1;
+					force=Clavier.entrerEntier("Quelle est votre force ?\n");
+					controlEmmenager.ajouterGaulois(nomVisiteur, force);
 					break;
 
 				default:
-					System.out
-							.println("Vous devez choisir le chiffre 1 ou 2 !");
+					System.out.println("Vous devez choisir le chiffre 1 ou 2 !");
 					break;
 				}
 			} while (choixUtilisateur != 1 && choixUtilisateur != 2);
@@ -40,6 +42,16 @@ public class BoundaryEmmenager {
 	}
 
 	private void emmenagerDruide(String nomVisiteur) {
-		//TODO a completer
+		System.out.println("Bienvenue druide " + nomVisiteur + ". ");
+		int force=-1;
+		force=Clavier.entrerEntier("Quelle est votre force ?\n");
+		int effetPotionMin=-1; int effetPotionMax=-2;
+		while (effetPotionMax<effetPotionMin) {
+			effetPotionMin=Clavier.entrerEntier("Quelle est la force de la potion la plus faible que vous produisez ?\n");
+			effetPotionMax=Clavier.entrerEntier("Quelle est la force de la potion la plus forte que vous produisez ?\n");
+			if (effetPotionMax<effetPotionMin) 
+				System.out.println("Attention Druide, vous vous être trompé entre le minimum et le maximum.\n");
+		}
+		controlEmmenager.ajouterDruide(nomVisiteur,force,effetPotionMin,effetPotionMax);
 	}
 }
